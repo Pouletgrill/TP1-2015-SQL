3a523c3a7dcfe1291f760254d01751069de84beb
PouletGrill <xavierb.2006@hotmail.com>
2015-02-04 12:01:02 -0500

    fss

--- TP1_ADO_NET/TP1_ADO_NET/Form1.Designer.cs
+++ TP1_ADO_NET/TP1_ADO_NET/Form1.Designer.cs
@@ -28,20 +28,45 @@
       /// </summary>
       private void InitializeComponent()
       {
+         this.BTN_Next = new System.Windows.Forms.Button();
+         this.BTN_Past = new System.Windows.Forms.Button();
          this.SuspendLayout();
+         // 
+         // BTN_Next
+         // 
+         this.BTN_Next.Location = new System.Drawing.Point(396, 255);
+         this.BTN_Next.Name = "BTN_Next";
+         this.BTN_Next.Size = new System.Drawing.Size(26, 23);
+         this.BTN_Next.TabIndex = 0;
+         this.BTN_Next.Text = "->";
+         this.BTN_Next.UseVisualStyleBackColor = true;
+         // 
+         // BTN_Past
+         // 
+         this.BTN_Past.Location = new System.Drawing.Point(364, 255);
+         this.BTN_Past.Name = "BTN_Past";
+         this.BTN_Past.Size = new System.Drawing.Size(26, 23);
+         this.BTN_Past.TabIndex = 1;
+         this.BTN_Past.Text = "<-";
+         this.BTN_Past.UseVisualStyleBackColor = true;
          // 
          // Form1
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(484, 329);
+         this.Controls.Add(this.BTN_Past);
+         this.Controls.Add(this.BTN_Next);
          this.Name = "Form1";
-         this.Text = "Form1";
+         this.Text = "Gestion";
          this.ResumeLayout(false);
 
       }
 
       #endregion
+
+      private System.Windows.Forms.Button BTN_Next;
+      private System.Windows.Forms.Button BTN_Past;
    }
 }
 
